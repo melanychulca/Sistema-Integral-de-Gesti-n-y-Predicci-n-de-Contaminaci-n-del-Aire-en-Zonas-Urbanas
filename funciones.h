a987ca7477ae6b96223a7cdca186e836a387a7e9
Melany Chulca <melany.chulca@udla.edu.ec>
2026-07-02 12:27:55 -0500

    Refactor funciones.h to optimize data structures

--- funciones.h
+++ funciones.h
@@ -5,10 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ZONAS 50
+#define MAX_ZONAS 20
 #define MAX_NOMBRE 50
 #define MAX_HISTORICO 30
+#define MAX_REGISTROS (MAX_ZONAS * MAX_HISTORICO)
 #define NOMBRE_ARCHIVO "zonas.dat"
+#define NOMBRE_ARCHIVO_REGISTROS "registros.dat"
+#define ALTITUD_QUITO 2850.0f
+#define FACTOR_PICO_PLACA 0.85f
 
 typedef struct {
     int dia;
@@ -17,9 +21,7 @@ typedef struct {
 } Fecha;
 
 typedef struct {
-    int id;
-    char nombre[MAX_NOMBRE];
-    int activo;
+    int id_zona;
     float co2;
     float so2;
     float no2;
@@ -28,68 +30,45 @@ typedef struct {
     float humedad;
     float temperaturaActual;
     Fecha fecha;
-    float historicoContaminacion[MAX_HISTORICO];
-    int diasHistorico;
+} Registro;
+
+typedef struct {
+    int id;
+    char nombre[MAX_NOMBRE];
+    int activo;
 } Zona;
 
 typedef struct {
     Zona zonas[MAX_ZONAS];
-    int cantidad;
+    Registro registros[MAX_REGISTROS];
+    int cantidadZonas;
+    int cantidadRegistros;
 } SistemaZonas;
-
 
 void inicializarSistema(SistemaZonas *sistema);
 
+int cargarDatosBinario(SistemaZonas *sistema, const char *nombreArchivoZonas);
+int guardarDatosBinario(const SistemaZonas *sistema, const char *nombreArchivoZonas);
 
-int cargarDatosBinario(SistemaZonas *sistema, const char *nombreArchivo);
-
-
-int guardarDatosBinario(const SistemaZonas *sistema, const char *nombreArchivo);
-
+int cargarZonasBinario(SistemaZonas *sistema, const char *nombreArchivo);
+int guardarZonasBinario(const SistemaZonas *sistema, const char *nombreArchivo);
+int cargarRegistrosBinario(SistemaZonas *sistema, const char *nombreArchivo);
+int guardarRegistroBinario(const Registro *registro, const char *nombreArchivo);
 
 int buscarZonaPorId(const SistemaZonas *sistema, int id);
-
-
 int buscarZonaPorNombre(const SistemaZonas *sistema, const char *nombre);
-
-
-int agregarOActualizarZona(SistemaZonas *sistema, const Zona *zona);
-
-
+int agregarZona(SistemaZonas *sistema, const Zona *zona);
 int eliminarZona(SistemaZonas *sistema, int id);
 
-
-void mostrarZona(const Zona *zona);
-
-
+void mostrarZona(const SistemaZonas *sistema, const Zona *zona);
 void mostrarEstadoActual(const SistemaZonas *sistema);
-
-
-void generarPrediccion24h(const SistemaZonas *sistema);
-
-
-float calcularPromedioHistorico(const Zona *zona);
-
-
-float calcularPromedioPonderado(const Zona *zona);
-
-// Obtiene un índice general de contaminación promedio a partir de los contaminantes.
-float obtenerIndiceContaminacion(const Zona *zona);
-
-// Muestra los promedios históricos de todas las zonas.
+void generarPrediccion24h(const SistemaZonas *sistema, const Fecha *fechaActual);
 void mostrarPromediosHistoricos(const SistemaZonas *sistema);
-
-// Busca y muestra zonas que contengan un texto en el nombre.
 void buscarZonasCoincidentes(const SistemaZonas *sistema, const char *texto);
-
-// Actualiza el registro histórico de contaminación de una zona.
-void actualizarRegistroHistorico(Zona *zona, float nuevoValor);
-
-// Registro y actualización de zonas desde la entrada estándar.
 void ingresarActualizarZona(SistemaZonas *sistema);
 
-// Validación y lectura de datos desde la entrada estándar.
 int validarCadena(const char *cadena);
+int validarValorFlotante(float valor, float min, float max);
 int leerEntero(const char *mensaje, int min, int max);
 float leerFlotante(const char *mensaje, float min, float max);
 
